f2ba9c1f4fd93402a3611260bc49f3fe10796fba
Raphaël LAZZARONI <xzeno@orange.fr>
2019-01-11 08:55:40 +0100

    readfile

---
+++ parser.py
@@ -0,0 +1,22 @@
+import numpy
+
+def	readdata(file,nbuser,nbfilm):
+    res = numpy.ones((nbuser,	nbfilm))
+    res = res*-1
+
+    fichier = open(file,	'rU')
+    lignes = fichier.readlines()
+
+    #	lecture	ligne	a	ligne
+    for line in lignes:
+        # print(line)
+        lineSplit = line.split("\t")
+        userid = int(lineSplit[0]) - 1
+        filmid = int(lineSplit[1]) - 1
+        score = int(lineSplit[2])
+        # print(str(userid)+"	"+str(filmid)+"	"+str(score))
+        res[userid, filmid] = score
+    return res
+
+
+print(readdata('ml-100k/u.data', 943, 1682))
