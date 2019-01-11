cd3cea3a67e8c7b50c727647bec4c47cb2bb32d7
Raphaël LAZZARONI <xzeno@orange.fr>
2019-01-11 10:04:48 +0100

    fix random predict

--- parser.py
+++ parser.py
@@ -20,21 +20,14 @@ def	readdata(file,nbuser,nbfilm):
         res[userid, filmid] = score
     return res
 
-def predict_random(nbuser, nbfilm):
-    res = numpy.zeros((nbuser, nbfilm))
-    for user in range(nbuser):
-        for film in range(nbfilm):
-            res[user][film] = (numpy.random.random()*4)+1
-    return res
-
 # RMSE 1
 def calc_rmse_predi_random(nbuser, nbvote, nbfilm, matrice):
-    score_random = (random.random() * 4) + 1
     sum_core = 0
     for index_user in range(0, nbuser):
         for index_film in range(0, nbfilm):
             score = matrice[index_user][index_film]
             if score > -1:
+                score_random = (random.random() * 4) + 1
                 sum_core += pow((score - score_random), 2)
     return math.sqrt(sum_core/ nbvote)
 
@@ -43,7 +36,5 @@ nbfilm = 1682
 nbvote = 100000
 
 matrice_recommandation = readdata('ml-100k/u.data', nbuser, nbfilm)
-matrice_predict_rand = predict_random(nbuser, nbfilm)
 #print(matrice_recommandation)
-#print(matrice_predict_rand)
 print(calc_rmse_predi_random(nbuser, nbvote, nbfilm, matrice_recommandation))
